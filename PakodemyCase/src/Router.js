b4096e6c83aa07577af3710af68547c5e29098fe
Zehra Tok <49477192+zehratok@users.noreply.github.com>
2022-12-19 00:21:53 +0300

    Create splash screen using react-native-splash-screen

--- PakodemyCase/src/Router.js
+++ PakodemyCase/src/Router.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import AppStack from "./navigations/appStack";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import SplashScreen from "react-native-splash-screen";
 
 const Router = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <Provider store={store}>
       <NavigationContainer>
